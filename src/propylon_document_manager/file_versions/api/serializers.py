426c9153abc1ed505f9d3e469faa5f7650d313cc
Jovan Stankovic <jovan.stankovic@gcore.com>
2025-08-03 13:45:01 +0200

    define serializers

--- src/propylon_document_manager/file_versions/api/serializers.py
+++ src/propylon_document_manager/file_versions/api/serializers.py
@@ -1,8 +1,49 @@
 from rest_framework import serializers
 
-from ..models import FileVersion
+from ..models import FileVersion, User
+
 
 class FileVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileVersion
-        fields = "__all__"
+        fields = [
+            "id",
+            "file_name",
+            "file_extension",
+            "version_number",
+            "file",
+            "url",
+            "cas_url",
+            "created_at",
+            "updated_at",
+            "user",
+        ]
+        read_only_fields = ["id", "cas_url", "created_at", "updated_at", "user", "file_extension", "version_number"]
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        return FileVersion.objects.create(user=request.user, **validated_data)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "password"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField()
